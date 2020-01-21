5307d9c1630387d4f2b1781e28b462f27a80a5e8
SHNecro <>
2020-01-20 21:43:10 -0800

    Added keystone unlock effect

---
+++ ShanghaiEXE/Effect/KeystoneUnlock.cs
@@ -0,0 +1,41 @@
+﻿using NSShanghaiEXE.InputOutput;
+using NSShanghaiEXE.InputOutput.Rendering;
+using SlimDX;
+using System.Drawing;
+
+namespace NSEffect
+{
+    internal class KeystoneUnlock : EffectBase
+    {
+        private const int FrameCount = 20;
+        private static readonly Rectangle SpriteRect = new Rectangle(840, 400, 140, 200);
+
+        public KeystoneUnlock(MyAudio s, Vector2 pd, Point posi)
+          : base(s, null, posi.X, posi.Y)
+        {
+            this.positionDirect = pd;
+            this.animationpoint.X = 0;
+        }
+
+        public override void Updata()
+        {
+            this.FlameControl(4);
+            if (!this.moveflame)
+                return;
+            if (this.animationpoint.X > frame)
+            {
+                this.flag = false;
+                return;
+            }
+            this.animationpoint.X++;
+        }
+
+        public override void Render(IRenderer dg)
+        {
+            this._rect = new Rectangle(SpriteRect.Left + this.animationpoint.X * SpriteRect.Width, SpriteRect.Top, SpriteRect.Width, SpriteRect.Height);
+            this._position = new Vector2(this.positionDirect.X + Shake.X, (float)(positionDirect.Y + (double)this.Shake.Y - SpriteRect.Height / 2));
+            this.color = Color.White;
+            dg.DrawImage(dg, "body25", this._rect, false, this._position, this.rebirth, this.color);
+        }
+    }
+}
