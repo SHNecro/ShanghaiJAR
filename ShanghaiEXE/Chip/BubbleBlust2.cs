a1c9ce2b88b6dbf721ee9d7ea275b4e306b44a16
SHNecro <>
2020-02-23 21:47:15 -0800

    Slightly nerfed bubbleblast, gives tiny final hit iframe so stunlock needs some rythem instead of A spam

--- ShanghaiEXE/Chip/BubbleBlust2.cs
+++ ShanghaiEXE/Chip/BubbleBlust2.cs
@@ -9,116 +9,119 @@ using System.Drawing;
 
 namespace NSChip
 {
-  internal class BubbleBlust2 : ChipBase
-  {
-    private const int shotend = 10;
-    private const int shotinterval = 4;
-
-    public BubbleBlust2(MyAudio s)
-      : base(s)
+    internal class BubbleBlust2 : ChipBase
     {
-      this.rockOnPoint = new Point(-3, 0);
-      this.number = 54;
-      this.name = NSGame.ShanghaiEXE.Translate("Chip.BubbleBlust2Name");
-      this.element = ChipBase.ELEMENT.aqua;
-      this.power = 20;
-      this.regsize = 22;
-      this.reality = 2;
-      this.subpower = 0;
-      this._break = false;
-      this.powerprint = true;
-      this.code[0] = ChipFolder.CODE.B;
-      this.code[1] = ChipFolder.CODE.D;
-      this.code[2] = ChipFolder.CODE.M;
-      this.code[3] = ChipFolder.CODE.V;
-      var information = NSGame.ShanghaiEXE.Translate("Chip.BubbleBlust2Desc");
-      this.information[0] = information[0];
-      this.information[1] = information[1];
-      this.information[2] = information[2];
-      this.Init();
-    }
+        private const int shotend = 10;
+        private const int shotinterval = 4;
 
-    public override void Action(CharacterBase character, SceneBattle battle)
-    {
-      character.animationpoint = BubbleBlust2.Animation(character.waittime);
-      if (character.waittime == 40)
-        base.Action(character, battle);
-      if (character.waittime % 8 == 0)
-        this.sound.PlaySE(MyAudio.SOUNDNAMES.vulcan);
-      if (character.waittime % 8 != 4)
-        return;
-      int num = this.power + this.pluspower;
-      character.parent.attacks.Add(this.Paralyze(new Vulcan(this.sound, character.parent, character.position.X + this.UnionRebirth(character.union), character.position.Y, character.union, this.Power(character), Vulcan.SHOT.Bubble, ChipBase.ELEMENT.aqua, false)));
-    }
+        public BubbleBlust2(MyAudio s)
+          : base(s)
+        {
+            this.rockOnPoint = new Point(-3, 0);
+            this.number = 54;
+            this.name = NSGame.ShanghaiEXE.Translate("Chip.BubbleBlust2Name");
+            this.element = ChipBase.ELEMENT.aqua;
+            this.power = 20;
+            this.regsize = 22;
+            this.reality = 2;
+            this.subpower = 0;
+            this._break = false;
+            this.powerprint = true;
+            this.code[0] = ChipFolder.CODE.B;
+            this.code[1] = ChipFolder.CODE.D;
+            this.code[2] = ChipFolder.CODE.M;
+            this.code[3] = ChipFolder.CODE.V;
+            var information = NSGame.ShanghaiEXE.Translate("Chip.BubbleBlust2Desc");
+            this.information[0] = information[0];
+            this.information[1] = information[1];
+            this.information[2] = information[2];
+            this.Init();
+        }
 
-    public static Point Animation(int waittime)
-    {
-      int[] interval = new int[12];
-      for (int index = 0; index < 12; ++index)
-        interval[index] = 4 * index;
-      int[] xpoint = new int[14]
-      {
-        5,
-        6,
-        5,
-        6,
-        5,
-        6,
-        5,
-        6,
-        5,
-        6,
-        5,
-        6,
-        5,
-        6
-      };
-      int y = 0;
-      return CharacterAnimation.Return(interval, xpoint, y, waittime);
-    }
+        public override void Action(CharacterBase character, SceneBattle battle)
+        {
+            character.animationpoint = BubbleBlust2.Animation(character.waittime);
+            if (character.waittime == 40)
+                base.Action(character, battle);
+            if (character.waittime % 8 == 0)
+                this.sound.PlaySE(MyAudio.SOUNDNAMES.vulcan);
+            if (character.waittime % 8 != 4)
+                return;
+            int num = this.power + this.pluspower;
+            var attack = this.Paralyze(new Vulcan(this.sound, character.parent, character.position.X + this.UnionRebirth(character.union), character.position.Y, character.union, this.Power(character), Vulcan.SHOT.Bubble, ChipBase.ELEMENT.aqua, false));
+            attack.invincibility = character.waittime > 40 - 8;
+            attack.invincibilitytimeA = 20;
+            character.parent.attacks.Add(attack);
+        }
 
-    public override void GraphicsRender(
-      IRenderer dg,
-      Vector2 p,
-      int c,
-      bool printgraphics,
-      bool printstatus)
-    {
-      if (printgraphics)
-      {
-        this._rect = new Rectangle(784, 0, 56, 48);
-        dg.DrawImage(dg, "chipgraphic2", this._rect, true, p, Color.White);
-      }
-      base.GraphicsRender(dg, p, c, printgraphics, printstatus);
-    }
+        public static Point Animation(int waittime)
+        {
+            int[] interval = new int[12];
+            for (int index = 0; index < 12; ++index)
+                interval[index] = 4 * index;
+            int[] xpoint = new int[14]
+            {
+                5,
+                6,
+                5,
+                6,
+                5,
+                6,
+                5,
+                6,
+                5,
+                6,
+                5,
+                6,
+                5,
+                6
+            };
+            int y = 0;
+            return CharacterAnimation.Return(interval, xpoint, y, waittime);
+        }
 
-    public override void IconRender(
-      IRenderer dg,
-      Vector2 p,
-      bool select,
-      bool custom,
-      int c,
-      bool noicon)
-    {
-      if (!noicon)
-      {
-        this._rect = this.IconRect(select);
-        dg.DrawImage(dg, "chipicon", this._rect, true, p, Color.White);
-      }
-      base.IconRender(dg, p, select, custom, c, noicon);
-    }
+        public override void GraphicsRender(
+          IRenderer dg,
+          Vector2 p,
+          int c,
+          bool printgraphics,
+          bool printstatus)
+        {
+            if (printgraphics)
+            {
+                this._rect = new Rectangle(784, 0, 56, 48);
+                dg.DrawImage(dg, "chipgraphic2", this._rect, true, p, Color.White);
+            }
+            base.GraphicsRender(dg, p, c, printgraphics, printstatus);
+        }
 
-    public override void Render(IRenderer dg, CharacterBase character)
-    {
-      this._rect = new Rectangle(4 * character.Wide, 2 * character.Height, character.Wide, character.Height);
-      this._position = new Vector2(character.positionDirect.X + Shake.X, character.positionDirect.Y + Shake.Y);
-      if (character.animationpoint.X == 6)
-      {
-        this._rect.X += 120;
-        this._position.X -= 2 * this.UnionRebirth(character.union);
-      }
-      dg.DrawImage(dg, "weapons", this._rect, false, this._position, character.union == Panel.COLOR.blue, Color.White);
+        public override void IconRender(
+          IRenderer dg,
+          Vector2 p,
+          bool select,
+          bool custom,
+          int c,
+          bool noicon)
+        {
+            if (!noicon)
+            {
+                this._rect = this.IconRect(select);
+                dg.DrawImage(dg, "chipicon", this._rect, true, p, Color.White);
+            }
+            base.IconRender(dg, p, select, custom, c, noicon);
+        }
+
+        public override void Render(IRenderer dg, CharacterBase character)
+        {
+            this._rect = new Rectangle(4 * character.Wide, 2 * character.Height, character.Wide, character.Height);
+            this._position = new Vector2(character.positionDirect.X + Shake.X, character.positionDirect.Y + Shake.Y);
+            if (character.animationpoint.X == 6)
+            {
+                this._rect.X += 120;
+                this._position.X -= 2 * this.UnionRebirth(character.union);
+            }
+            dg.DrawImage(dg, "weapons", this._rect, false, this._position, character.union == Panel.COLOR.blue, Color.White);
+        }
     }
-  }
 }
 
