64cdf641134aed2a3972fd4269e43d38f46e72d9
SHNecro <>
2020-05-02 00:52:44 -0700

    Moved messages to external file not requiring recompilation to modify

--- ShanghaiEXE/Messages/AllMessages.cs
+++ ShanghaiEXE/Messages/AllMessages.cs
@@ -1,5 +1,7 @@
 using Common;
+using NSGame;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace Messages
 {
@@ -20,24 +22,46 @@ namespace Messages
         static AllMessages()
         {
             messages = new Dictionary<MessageType, Dialogue[][]>();
-            messages[MessageType.ShanghaiInfo] = AllMessages.GetShanghaiInfo();
-            messages[MessageType.AliceInfo] = AllMessages.GetAliceInfo();
-            messages[MessageType.ShanghaiRequestInfo] = AllMessages.GetShanghaiRequestInfo();
-            messages[MessageType.AliceRequestInfo] = AllMessages.GetAliceRequestInfo();
-            messages[MessageType.HumorInfo] = AllMessages.GetHumorInfo();
-            messages[MessageType.UNKNOWN1] = AllMessages.GetUNKNOWN1();
-            messages[MessageType.UNKNOWN2] = AllMessages.GetUNKNOWN2();
-            messages[MessageType.EirinCallInfo] = AllMessages.GetEirinCallInfo();
-            messages[MessageType.RequestBoard] = AllMessages.GetRequestBoard();
-            messages[MessageType.RequestBoardComplete] = AllMessages.GetRequestBoardComplete();
-            messages[MessageType.GenBoard] = AllMessages.GetGenBoard();
-            messages[MessageType.UniversityBoard] = AllMessages.GetUniversityBoard();
-            messages[MessageType.EienBoard] = AllMessages.GetEienBoard();
-            messages[MessageType.UnderBoard] = AllMessages.GetUnderBoard();
-            messages[MessageType.UnderBattleBoard] = AllMessages.GetUnderBattleBoard();
-            messages[MessageType.UNKNOWN3] = AllMessages.GetUNKNOWN3();
+            messages[MessageType.ShanghaiInfo] = AllMessages.LoadMessages("ShanghaiInfo.xml");
+            messages[MessageType.AliceInfo] = AllMessages.LoadMessages("AliceInfo.xml");
+            messages[MessageType.ShanghaiRequestInfo] = AllMessages.LoadMessages("ShanghaiRequestInfo.xml");
+            messages[MessageType.AliceRequestInfo] = AllMessages.LoadMessages("AliceRequestInfo.xml");
+            messages[MessageType.HumorInfo] = AllMessages.LoadMessages("HumorInfo.xml");
+            messages[MessageType.UNKNOWN1] = AllMessages.LoadMessages("UNKNOWN1.xml");
+            messages[MessageType.UNKNOWN2] = AllMessages.LoadMessages("UNKNOWN2.xml");
+            messages[MessageType.EirinCallInfo] = AllMessages.LoadMessages("EirinCallInfo.xml");
+            messages[MessageType.RequestBoard] = AllMessages.LoadMessages("RequestBoard.xml");
+            messages[MessageType.RequestBoardComplete] = AllMessages.LoadMessages("RequestBoardComplete.xml");
+            messages[MessageType.GenBoard] = AllMessages.LoadMessages("GenBoard.xml");
+            messages[MessageType.UniversityBoard] = AllMessages.LoadMessages("UniversityBoard.xml");
+            messages[MessageType.EienBoard] = AllMessages.LoadMessages("EienBoard.xml");
+            messages[MessageType.UnderBoard] = AllMessages.LoadMessages("UnderBoard.xml");
+            messages[MessageType.UnderBattleBoard] = AllMessages.LoadMessages("UnderBattleBoard.xml");
+            messages[MessageType.UNKNOWN3] = AllMessages.LoadMessages("UNKNOWN3.xml");
         }
         
         public static Dialogue[] GetMessage(MessageType msgType, int number) => messages[msgType][number];
+
+        private static Dialogue[][] LoadMessages(string sourceFile)
+        {
+            var languageDoc = new XmlDocument();
+            languageDoc.Load($"language/data/{sourceFile}");
+
+            var text = languageDoc.SelectNodes("data/Message");
+            var messageArray = new Dialogue[text.Count][];
+            foreach (XmlNode message in text)
+            {
+                var index = int.Parse(message.Attributes["Index"].Value);
+                var dialogues = message.ChildNodes;
+                messageArray[index] = new Dialogue[dialogues.Count];
+                var nodeNumber = 0;
+                foreach (XmlNode dialogue in message)
+                {
+                    messageArray[index][nodeNumber++] = ShanghaiEXE.Translate(dialogue.Attributes["Key"].Value);
+                }
+            }
+
+            return messageArray;
+        }
     }
 }
