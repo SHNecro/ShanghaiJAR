9acee5a8ac74eda1375bfd937c73525801e58508
SHNecro <>
2021-01-14 00:16:22 -0800

    Fixed settings addoncount

--- MapEditor/Core/Settings.cs
+++ MapEditor/Core/Settings.cs
@@ -38,7 +38,7 @@ namespace MapEditor.Core
 
         public int EnemyCount { get; set; } = 85;
         public int ChipCount { get; set; } = 431;
-        public int AddOnCount { get; set; } = 95;
+        public int AddOnCount { get; set; } = 98;
         public int InteriorCount { get; set; } = 51;
         public int BackgroundCount { get; set; } = 39;
 
