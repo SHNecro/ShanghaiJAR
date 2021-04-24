285faff16038be919c22d09e538750b8c712873f
Shadowretro1 <shadowretro1@gmail.com>
2021-04-24 16:20:06 -0600

    Adjusted ShanghaiDS to be 'fair'

--- ShanghaiEXE/Enemy/ShanghaiDS.cs
+++ ShanghaiEXE/Enemy/ShanghaiDS.cs
@@ -11,12 +11,50 @@ namespace NSEnemy
 {
     internal class ShanghaiDS : ChipUsingNaviBase
     {
+
+        protected ChipFolder[] hand;
+        protected int deckLoc;
+        protected int deckSize;
+
         public ShanghaiDS(IAudioEngine s, SceneBattle p, int pX, int pY, byte n, Panel.COLOR u, byte v)
             : base(s, p, pX, pY, n, u, v, 3000, "ShanghaiDS", "ShanghaiDS")
         {
 
             /*tst*/
 
+
+
+
+        }
+
+        /*
+        private void shuffleDeck(this Random rng, T[] array)
+        {
+            //
+            var rng = new Random();
+
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = rng.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
+
+        }
+        */
+
+        private void Shuffle<T>(T[] array)
+        {
+            int n = array.Length;
+            while (n > 1)
+            {
+                int k = Random.Next(n--);
+                T temp = array[n];
+                array[n] = array[k];
+                array[k] = temp;
+            }
         }
 
         protected override void InitializeChips()
@@ -35,13 +73,34 @@ namespace NSEnemy
                 this.chips[i].SettingChip(playerFolder[equipFolderIndex, i].chip.number);
                 this.chips[i].codeNo = playerFolder[equipFolderIndex, i].codeNo;
             }
+
+
+            var rng = new Random();
+            Shuffle(this.chips);
+            Shuffle(this.chips);
+
+            this.deckSize = this.chips.Length;
+            this.deckLoc = 0;
+
         }
 
         protected override void SetUsedChip()
         {
             base.SetUsedChip();
 
-            this.usechip = Random.Next(this.chips.Length);
+            //this.usechip = Random.Next(this.chips.Length);
+
+            this.usechip = this.deckLoc;
+            this.deckLoc++;
+            if (this.deckLoc >= this.deckSize)
+            {
+                //
+                this.deckLoc = 0;
+
+            }
+
+
+
         }
     }
 }
