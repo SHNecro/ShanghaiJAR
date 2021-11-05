23b8113e7393d41f3146436283185d1de2da0249
SHNecro <>
2021-11-04 21:53:23 -0700

    Added catch-all exception printing, deleted unused/broken networking leftover

--- ShanghaiEXE/Game/Program.cs
+++ ShanghaiEXE/Game/Program.cs
@@ -9,17 +9,25 @@ namespace NSGame
         [STAThread]
         private static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            using (ShanghaiEXE form = new ShanghaiEXE())
+            try
             {
-                Loading loading = new Loading(form);
-                loading.Show();
-                new Thread(new ThreadStart(loading.MainLoop)).Start();
-                form.loading = loading;
-                form.InitializeData();
-                while (form.Created || loading.Created)
-                    form.MainLoop();
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                using (ShanghaiEXE form = new ShanghaiEXE())
+                {
+                    Loading loading = new Loading(form);
+                    loading.Show();
+                    new Thread(new ThreadStart(loading.MainLoop)).Start();
+                    form.loading = loading;
+                    form.InitializeData();
+                    while (form.Created || loading.Created)
+                        form.MainLoop();
+                }
+            }
+            catch (Exception e)
+            {
+                var messageText = e.ToString();
+                MessageBox.Show(messageText, "Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
